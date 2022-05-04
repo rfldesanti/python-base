6545fc66ad95f011955f35469101f78c2cd8ed77
Rafael <rafaelsr2018@gmail.com>
2022-05-03 22:52:09 -0300

    Import do módulo os e adição de mensagens em outras línguas

--- hello.py
+++ hello.py
@@ -1,5 +1,35 @@
 #!/usr/bin/env python3
-# Este programa imprime "Hello, World!"
+"""Hello World Multi Linguas
 
-print("Hello, World!")
+Dependendo da lingua configurada no ambiente, o programa exibe a mensagem
+correspondente.
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada. Ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Rafael"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!" 
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
 
