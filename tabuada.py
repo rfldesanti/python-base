2153bfe30b71c42d032cb88e822d1ab438f2f0e2
Rafael <rafaelsr2018@gmail.com>
2022-05-08 18:12:21 -0300

    Add tabuada.py

---
+++ tabuada.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Imprime a tabuada do 1 ao 10.
+
+Tabuada do 1
+1
+2
+...
+____________
+Tabuada do 2
+2
+4
+...
+____________
+"""
+__version__ = "0.1.0"
+__author__ = "Rafa"
+
+#numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numeros = list(range(1, 11))
+
+# Iterable
+
+# para cada número em números
+for numero in numeros:
+    print("Tabuada do: ", numero)
+    for outro_numero in numeros:
+        print(numero * outro_numero)
+    print("----------")
